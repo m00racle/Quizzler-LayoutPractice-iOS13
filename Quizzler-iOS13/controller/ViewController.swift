9a9b2354ad0fed7be04ceb5643e8b45d55add3b7
m00racle <yanuar.prakosa@gmail.com>
2021-09-12 14:45:35 +0700

    add updateUI function in ViewController

--- Quizzler-iOS13/controller/ViewController.swift
+++ Quizzler-iOS13/controller/ViewController.swift
@@ -21,12 +21,40 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
     }
     
+    let host:QuizHost = QuizBot(pool: Questions_Eng().pool)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
-
+    
+    func updateUI() {
+//        update the UI
+        scoreLabel.text = "score: " + host.showScore()
+        quizScreen.text = host.ask()
+        topButton.backgroundColor = UIColor.clear
+        midButton.backgroundColor = UIColor.clear
+        botButton.backgroundColor = UIColor.clear
+        
+        let choices:[String] = host.passChoices()
+        
+        if choices.count < 3 {
+//            true or false buttons
+            topButton.isEnabled = false
+            topButton.isHidden = true
+            
+            midButton.setTitle(choices[0], for: UIControl.State.normal)
+            botButton.setTitle(choices[1], for: UIControl.State.normal)
+        } else {
+//            multiple choice buttons
+            topButton.isEnabled = true
+            topButton.isHidden = false
+            topButton.setTitle(choices[0], for: UIControl.State.normal)
+            midButton.setTitle(choices[1], for: UIControl.State.normal)
+            botButton.setTitle(choices[2], for: UIControl.State.normal)
+        }
+    }
 
 }
 
